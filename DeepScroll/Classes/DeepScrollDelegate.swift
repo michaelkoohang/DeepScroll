4e7554dee9f164993b2525e79cb184623fd9e94b
Michael Koohang <michael.s.k1001@gmail.com>
2019-11-10 01:23:04 -0500

    Added scroll lane indicator animation

--- DeepScroll/Classes/DeepScrollDelegate.swift
+++ DeepScroll/Classes/DeepScrollDelegate.swift
@@ -16,12 +16,40 @@ public class LanedScrollerDelegate: NSObject, UITableViewDelegate {
         self.init(lanedScrollerId: 0)
     }
     
-    
     init(lanedScrollerId: Int) {
         self.lanedScrollerId = lanedScrollerId
         super.init()
     }
+        
+    var leftLane: UIView = {
+        let v = UIView()
+        v.backgroundColor = .lightGray
+        v.alpha = 0
+        v.isUserInteractionEnabled = false
+        v.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height)
+        v.translatesAutoresizingMaskIntoConstraints = false
+        return v
+    }()
     
+    var centerLane: UIView = {
+        let v = UIView()
+        v.backgroundColor = .lightGray
+        v.alpha = 0
+        v.isUserInteractionEnabled = false
+        v.frame = CGRect(x: UIScreen.main.bounds.width / 3, y: 0, width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height)
+        v.translatesAutoresizingMaskIntoConstraints = false
+        return v
+    }()
+    
+    var rightLane: UIView = {
+        let v = UIView()
+        v.backgroundColor = .lightGray
+        v.alpha = 0
+        v.isUserInteractionEnabled = false
+        v.frame = CGRect(x: UIScreen.main.bounds.width / 3 * 2, y: 0, width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height)
+        v.translatesAutoresizingMaskIntoConstraints = false
+        return v
+    }()
     
     public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
@@ -36,6 +64,10 @@ public class LanedScrollerDelegate: NSObject, UITableViewDelegate {
         let touchLocation = scrollView.panGestureRecognizer.location(in: containerView)
         let touchX = touchLocation.x
         var scrollLaneChanged = false
+        
+        UIApplication.shared.windows.first?.rootViewController?.view.addSubview(leftLane)
+        UIApplication.shared.windows.first?.rootViewController?.view.addSubview(centerLane)
+        UIApplication.shared.windows.first?.rootViewController?.view.addSubview(rightLane)
         
         if (touchSection == .none) {
             touchSection = .right
@@ -59,6 +91,26 @@ public class LanedScrollerDelegate: NSObject, UITableViewDelegate {
         if scrollLaneChanged {
             hapticfeedback.impactOccurred()
             sendScrollStateNotification(for: lanedScrollerId, touchSection: touchSection)
+            switch touchSection {
+            case .left:
+                UIView.animate(withDuration: 0.4) {
+                    self.leftLane.alpha = 0.8
+                    self.leftLane.alpha = 0
+                }
+            case .center:
+                UIView.animate(withDuration: 0.4) {
+                    self.centerLane.alpha = 0.8
+                    self.centerLane.alpha = 0
+                }
+            case .right:
+                UIView.animate(withDuration: 0.4) {
+                    self.rightLane.alpha = 0.8
+                    self.rightLane.alpha = 0
+                }
+            default:
+                break
+            }
         }
     }
+
 }
